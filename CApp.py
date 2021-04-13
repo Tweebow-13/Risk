804a36cba2217b8bb965eb7359c4dfa0ac9791bc
Toto <t.rugliano@gmail.com>
2021-04-13 11:03:31 +0200

    background scaleable

--- CApp.py
+++ CApp.py
@@ -6,7 +6,7 @@ import os
 class CApp(cevent.CEvent):
     def __init__(self):
         self._running = True
-        self.size = [self.weight, self.height] = [500, 317]
+        self.size = [self.weight, self.height] = [600, 400]
         #self.bg = None
 
     def on_exit(self):
@@ -14,28 +14,24 @@ class CApp(cevent.CEvent):
 
     def on_init(self):
         pg.init()
-        self._display_surf = pg.display.set_mode(self.size, pg.HWSURFACE | pg.DOUBLEBUF)
-        #bg = pg.display.set_mode(self.size)
+        self._display_surf = pg.display.set_mode(self.size, pg.RESIZABLE | pg.SCALED)
+        #self._display_surf = pg.display.set_mode(self.size, pg.HWSURFACE | pg.DOUBLEBUF)
         main_dir = os.path.split(os.path.abspath(__file__))[0]
         
-        # load image and quadruple
         imagename = os.path.join(main_dir, "data", "map.png")
-        bitmap = pg.image.load(imagename)
-        # bitmap = pg.transform.scale2x(bitmap)
-        # bitmap = pg.transform.scale2x(bitmap)
-        bitmap = bitmap.convert()
+        picture = pg.image.load(imagename)
+        picture = pg.transform.scale(picture, self.size)
+        picture = picture.convert()
         
-        self._display_surf.fill((0, 0, 255))
-        self._display_surf.blit(bitmap, [0, 0])
-        pg.display.flip()
+        self._display_surf.fill((0, 255, 255))
+        self._display_surf.blit(picture, [0, 0])
 
-        #self._display_surf = pg.display.set_mode(self.size, pg.HWSURFACE | pg.DOUBLEBUF)
         self._running = True
 
     def on_loop(self):
         pass
     def on_render(self):
-        pass
+        pg.display.update()
     def on_cleanup(self):
         pg.quit()
  
@@ -48,4 +44,5 @@ class CApp(cevent.CEvent):
                 self.on_event(event)
             self.on_loop()
             self.on_render()
+
         self.on_cleanup()
